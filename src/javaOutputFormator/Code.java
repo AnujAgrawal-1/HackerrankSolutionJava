a327951ee6149f7341d652b333731819d1d7aea3
Anuj Agrawal <anujagrawal308@gmail.com>
2021-04-18 09:00:12 +0530

    initial commit

--- src/javaOutputFormator/Code.java
+++ src/javaOutputFormator/Code.java
@@ -2,8 +2,6 @@
 
 To get you started, a portion of the solution is provided for you in the editor; you must format and print the input to complete the solution.
 
-Input Format
-
 Every line of input will contain a String followed by an integer.
 Each String will have a maximum of  alphabetic characters, and each integer will be in the inclusive range from  to .
 
